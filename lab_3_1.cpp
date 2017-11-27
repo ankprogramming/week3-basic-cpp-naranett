a6d576b1ac7f066294ec9d059ab54eceafc31707
Unknown <jaebsai_2542@hotmail.com>
2017-11-27 18:09:02 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Miss.Naranet Yungklang" << endl;
+  cout << "Nickname Sai" << endl;
+  cout << "Sex female" << endl;
+  cout << "Birthday 13 March 1999" << endl;
+  cout << "Hobbies watch a movie" << endl;
+  cout << "Status single" << endl;
   return 0;
 }

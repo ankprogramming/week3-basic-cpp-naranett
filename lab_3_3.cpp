d2f41b22b1d246f26bb4ab9968536cf6b16365e0
Unknown <jaebsai_2542@hotmail.com>
2017-11-28 11:50:51 +0700

    ทำข้อ 3 เสร็จ

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
-  
+
+    const float pi = 3.14315926536;
+    float r;
+    float area;
+    int h=15;
+    //วงกลม
+
+    cout << "Please enter radius : ";
+    cin >> r;
+    cout << "Area of Circle : " << pi * r * r << endl;
+
+    //เส้นรอบ
+    cout << "Area of Circumference : " << 2*pi * r << endl;
+
+    //ทรงกระบอก
+    cout << "Area of Cylinder : " << pi*(r * r) * h << endl;
+
   return 0;
 }

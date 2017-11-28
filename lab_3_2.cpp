9d6bc6b3f24c01ae870ae65df0689cba2aacabb8
Unknown <jaebsai_2542@hotmail.com>
2017-11-28 11:20:03 +0700

    ข้อ2เสร็จ

--- lab_3_2.cpp
+++ lab_3_2.cpp
@@ -10,12 +10,6 @@ int main() {
    cout << "Size of int : " << sizeof(int) << endl;
    cout << "Size of signed int : " << sizeof(signed int) << endl;
    cout << "Size of unsigned int : " << sizeof(unsigned int) << endl;
-<<<<<<< Updated upstream
-   cout << "Size of short signed int : " << sizeof(short signed int) << endl;
-   cout << "Size of short unsigned int : " << sizeof(short unsigned int) << endl;
-   cout << "Size of long int : " << sizeof(long int) << endl;
-   cout << "Size of float : " << sizeof(float) << endl;
-=======
    cout << "Size of short int : " << sizeof(short int) << endl;
    cout << "Size of signed short int : " << sizeof(signed short int) << endl;
    cout << "Size of unsigned short int : " << sizeof(unsigned short int) << endl;
@@ -27,8 +21,6 @@ int main() {
    cout << "Size of long double : " << sizeof(long double) << endl;
    cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
    cout << "Size of bool : " << sizeof(bool) << endl;
->>>>>>> Stashed changes
-
 
    // เขียนโปรแกรมเพิ่มเพื่อหาช่วงของค่าที่เก็บได้
    cout << "Min of char : " << numeric_limits<char>::min() << endl;
